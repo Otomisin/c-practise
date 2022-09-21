02814b08833ffd8e141135a372b830cf55272f0c
otomisin <orenaiket@yahoo.com>
2022-09-21 18:48:03 +0100

    new

---
+++ alx-test/arrays1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+     int mygrade [] = {23, 50, 75, 100,};
+     printf("%d ",mygrade[0]);
+
+     // index = 0 based number
+     // element =
+     // size =
+     return 0;
+}
