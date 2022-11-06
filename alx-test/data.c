725d95a49faeb8b9d9cfeafa0841395c9318efac
otomisin <orenaiket@yahoo.com>
2022-11-06 15:17:32 +0100

    news

--- alx-test/data.c
+++ alx-test/data.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
-#include <conio.h>
-void main ()
-	int a = 32768, 
+
+int main()
+{
+	int a = 9;
+	int b = -9;
+	int c = 89U;
+	long int d = 99998L;
+
+	printf("Integer value with postive data: %d\n", a);
+	printf("Integer value with negative data: %d\n", b);
+	printf("integer value with negative value: %u\n", c);
+	printf("integer value with a long int data: %ld\n", d);
+
+	return 0;
+
+}
