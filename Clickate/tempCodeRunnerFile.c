9474e71dcdd68d0d56065fa360be4afa9dd7572b
otomisin <orenaiket@yhaoo.com>
2022-11-22 06:35:28 +0000

    practise file

---
+++ Clickate/tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+/*
+User input
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int age;
+
+    printf ("How old are you: ");
+    scanf  ("%d", &age);
+
+    printf("You are %d years old", age );
+
+    return 0;
+}
