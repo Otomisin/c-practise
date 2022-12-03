ee703a87acb6252eb22f5c52c0b27e100b8dc7e2
otomisin <orenaiket@yahoo.com>
2022-12-03 15:41:12 +0100

    checking

---
+++ pyhton_Note/testcode.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+character = 'd'
+print(ord(character))
---
+++ w3sch and Python Notes/New/Try.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def my_function():
+    print("In my function")
+
+my_function
+
+def my_function(counter=89):
+     print("Counter: {}".format(counter))
+my_function()
+
+def my_function(counter=89):
+    return counter + 1
+print(my_function())
+
+def my_function(counter):
+    print("Counter: {}".format(counter))
+
+my_function(12)
