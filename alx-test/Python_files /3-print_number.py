30f3fc014b387dd605eaf71c9cfef03ad3026d2e
Otomisin <orenaiket@yahoo.com>
2022-12-12 18:41:33 +0000

    hope

--- alx-test/Python_files /3-print_number.py
+++
@@ -1,3 +0,0 @@
-def print_last_digit(number):
-    number = number % 10
-    return number
--- pyhton_Note/22_stacks and queues**.py
+++
@@ -1,4 +0,0 @@
-"""
-https://medium.com/analytics-vidhya/how-to-use-lists-as-stacks-and-queues-in-python-f680a84b792d
-https://www.youtube.com/watch?v=IxQHWt0GpsU&ab_channel=CalebCurry
-"""
