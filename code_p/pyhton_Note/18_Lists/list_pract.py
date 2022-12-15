126059c1cc5c777be768fb46736d12cc7354fcc0
Otomisin <orenaiket@yahoo.com>
2022-12-15 09:10:53 +0100

    man

---
+++ code_p/pyhton_Note/18_Lists/list_pract.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry"]
+
+# Add to a list
+thislist.insert( 3,"man")
+print(thislist)
+
+# Change variables in a list
+thislist[1:2]= ["Mango", "Pinaapple"]
+print(thislist)
--- pyhton_Note/00_Others.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-'''
-To iterate over the indices of a sequence, you can combine range() and len() as follows:
-'''
-a = ['Mary', 'had', 'a', 'little', 'lamb']
-for i in range (len(a)):
-     print(i,a[i])
