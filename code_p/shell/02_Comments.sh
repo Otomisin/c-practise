24783fde7ccc84b08ec323e332aac38478556bc0
Otomisin <orenaiket@yahoo.com>
2022-12-17 08:13:57 +0100

    Shells

---
+++ code_p/shell/02_Comments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# A single line is wrtten this way
+
+#Here is a multiple line comment
+<<comment 
+I am making a comment here to see the outcome
+comment
+
+echo No comment
+echo "This is a double quote"
+echo 'This is a single quote'
---
+++ code_p/shell/Variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#This is a comment
+#
+NAME="Tosin"
+echo "My name is $NAME"
