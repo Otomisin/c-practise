db030d6279f1be2e3fbff87ff2c1970649a66502
Otomisin <orenaiket@yahoo.com>
2022-12-23 08:05:41 +0000

    A day at a time

--- Practise_code/Pyhton_practise/15_Loop/tempCodeRunnerFile.py
+++ Practise_code/Pyhton_practise/15_Loop/tempCodeRunnerFile.py
@@ -1,3 +1,8 @@
-          if extra_cheese == "Y":
-               bill += 1
-               print(f"Your final bill: ${bill}.")
+               if door == "red":
+               print("It's a room full of fire. Game over. ")
+               elif door == "yellow":
+                    print("You found the treaure! You win!")
+               elif door == "blue":
+                    print("You got attacked by an antry trout. Game over. ")
+               else:
+                    print("You fell into a hole :( ")
