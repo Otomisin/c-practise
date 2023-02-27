fe0a2f840c974a3ce2ea7f4bcc6ae25e441d5b55
otomisin <orenaiket@yahoo.com>
2023-02-27 07:42:48 +0000

    modules

--- temp.R
+++ temp.R
@@ -1,11 +1,3 @@
-hell0 <- c(1, 2, 3)
-print ("hello")
-
-mdf <- data.frame("cat" = c("A","B","C"), values = c(12,10,15))
-# View(mdf)
-
-mygraph <- ggplot (mdf, aes(x = cat , y = values)) +
-     geom_bar(stat = "Identity", color = "#f24747", fill = "#19af46")+
-     ggtitle("My Test Bar Graph")
-
-mygraph
+#!/usr/bin/python3
+import mod
+dir()
--- tempCodeRunnerFile.R
+++ tempCodeRunnerFile.R
@@ -1,2 +1,2 @@
-hell0 <- c(1,2,3)
-print ("hello")
+import mod
+dir()
