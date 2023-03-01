9ad472a59b08e6f6c6e7db1d23a02fbac4d322ab
otomisin <orenaiket@yahoo.com>
2023-03-01 08:52:55 +0000

    OOP

--- Practise_code/Pyhton_practise/24_OOP/Test.py
+++ Practise_code/Pyhton_practise/24_OOP/Test.py
@@ -1,16 +1,18 @@
-"""Defines a Rectangle class."""
+class Parent1:
+    def say_hello(self):
+        print("Hello from Parent1!")
 
+class Parent2:
+    def say_hello(self):
+        print("Hello from Parent2!")
 
-class Rectangle:
-    """Represent a rectangle."""
-    def __init__(self, width, height, value):
-        self._width = width
-        self._height = height
+class Child(Parent1, Parent2):
+    def say_hello(self):
+        # Call the say_hello method from Parent1 using super()
+        super(Parent1, self).say_hello()
+        # Call the say_hello method from Parent2 using super()
+        super(Parent2, self).say_hello()
 
-    def width(self):
-        return self.value
-    
-    def height(self):
-    return self.height
-    
-    #pro
+# Create an instance of Child and call its say_hello method
+c = Child()
+c.say_hello()
