5b03c1f6f15b6a7d4de908ebded54f1fab8fdc73
otomisin <orenaiket@yahoo.com>
2023-03-11 06:17:47 +0000

    practise

--- Practise_code/Pyhton_practise/26_input-output/Test.py
+++ Practise_code/Pyhton_practise/26_input-output/Test.py
@@ -1,10 +1,6 @@
+import json
 
-with open('test.txt', 'r') as f:
-     f_content1 = f.read()
-     f_content2 = f.readline() # This reads one line for 
-     f_content3 = f.readlines()
+with open("jsonsample.json", "r") as read_file:
+     data = json.load(read_file)
 
-     print(f_content1)
-     print(f_content2, end=' ') # ' ' would remove the spaces at the end of the print line
-     print(f_content3)
-     
+print(data)
