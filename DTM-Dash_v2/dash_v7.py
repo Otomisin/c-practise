7a18c1dcd06131a0199cd691a58611189a6b25ae
Oluwatosin Orenaike <orenaiket@yahoo.com>
2024-05-13 14:11:22 +0100

    Update dash_v7.py

--- DTM-Dash_v2/dash_v7.py
+++ DTM-Dash_v2/dash_v7.py
@@ -1,38 +1,17 @@
 import streamlit as st
+import os, sys
+
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import FirefoxOptions
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+browser = webdriver.Firefox(options=opts)
 
-def setup_webdriver():
-    # Specify the path to ChromeDriver explicitly (adjust this path as per your setup)
-    chrome_driver_path = '/path/to/chromedriver'  # Adjust this path
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
-    # Create a new instance of the Chrome driver
-    service = Service(executable_path=chrome_driver_path)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    return driver
-
-def check_website_status():
-    driver = setup_webdriver()
-    url = 'https://dtm.iom.int/reports'
-    driver.get(url)
-    time.sleep(3)  # Allow time for the page to load
-    status = driver.title
-    driver.quit()
-    return status
-
-def app():
-    st.title('Website Status Checker with Selenium')
-    page_title = check_website_status()
-    if page_title:
-        st.success(f'The website was reached successfully. Page Title: {page_title}')
-    else:
-        st.error('Failed to reach the website.')
-
-if __name__ == '__main__':
-    app()
+browser.get('http://example.com')
+st.write(browser.page_source)
